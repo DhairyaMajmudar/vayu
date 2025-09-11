f7dac967bb2e49fd9da15d79e1e8836b3d0b86f6
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-09-12 00:54:53 +0530

    FEA: Index File to export all components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { CallToAction } from "./CallToAction";
+export { FeaturesSection } from "./FeaturesSection";
+export { Header } from "./Header";
+export { HeroSection } from "./HeroSection";
+export { Logo } from "./Logo";
+export { TestimonialsSection } from "./TestimonialsSection";
