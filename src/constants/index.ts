466fe38783a1afc6f8a41808c836b5c67bc0b23e
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2025-09-12 11:21:17 +0530

    FEA: Add indian holidays constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,68 @@
+export const indianHolidays = [
+  {
+    title: "Republic Day",
+    start: new Date(2025, 0, 26),
+    end: new Date(2025, 0, 26),
+    isHoliday: true,
+  },
+  {
+    title: "Holi",
+    start: new Date(2025, 2, 14),
+    end: new Date(2025, 2, 14),
+    isHoliday: true,
+  },
+  {
+    title: "Ram Navami",
+    start: new Date(2025, 3, 7),
+    end: new Date(2025, 3, 7),
+    isHoliday: true,
+  },
+  {
+    title: "Good Friday",
+    start: new Date(2025, 3, 18),
+    end: new Date(2025, 3, 18),
+    isHoliday: true,
+  },
+  {
+    title: "Buddha Purnima",
+    start: new Date(2025, 4, 13),
+    end: new Date(2025, 4, 13),
+    isHoliday: true,
+  },
+  {
+    title: "Eid al-Fitr",
+    start: new Date(2025, 4, 24),
+    end: new Date(2025, 4, 24),
+    isHoliday: true,
+  },
+  {
+    title: "Independence Day",
+    start: new Date(2025, 7, 15),
+    end: new Date(2025, 7, 15),
+    isHoliday: true,
+  },
+  {
+    title: "Gandhi Jayanti",
+    start: new Date(2025, 9, 2),
+    end: new Date(2025, 9, 2),
+    isHoliday: true,
+  },
+  {
+    title: "Dussehra",
+    start: new Date(2025, 9, 30),
+    end: new Date(2025, 9, 30),
+    isHoliday: true,
+  },
+  {
+    title: "Diwali",
+    start: new Date(2025, 10, 19),
+    end: new Date(2025, 10, 19),
+    isHoliday: true,
+  },
+  {
+    title: "Christmas",
+    start: new Date(2025, 11, 25),
+    end: new Date(2025, 11, 25),
+    isHoliday: true,
+  },
+];
